c7fed69a4eb487b0aa8b1b486714f8fe876874f6
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-10-26 18:06:01 +0000

    added more routes

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'shotgun'
 
 get '/' do
   "Hello, World!"
+end
+
+get '/secret' do
+ "Gotta pay the toll to get in the boy's hole"
+end
+
+get '/paddyspub' do
+  "Salt the snail"
+end
+
+get '/deon' do
+  "Kung-fu master"
+end
+
+get '/aaron' do
+  "do muay thai"
 end
